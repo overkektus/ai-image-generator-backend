4e6a80652aacd0e6e80ed9cc978bd66046d2ef64
Yahor Piskunou <egor.piskunou@gmail.com>
2023-03-26 15:13:50 +0200

    add 'getChatGPTSuggestion' endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,29 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import * as dotenv from "dotenv";
+import { Configuration, OpenAIApi } from "openai";
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+app.get("/api/getChatGPTSuggestion", async (req: Request, res: Response) => {
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt:
+      "Write a random text prompt for DALL•E to generate an image, this prompt will be shown to the user, include details such as the genre and what type of painting it should be options can include: oil painting. watercolor, photo-realistic, 4k, abstract, modern, black and white etc. Do not wrap the answer in quotes.",
+    max_tokens: 100,
+    temperature: 0.8,
+  });
+  const responseText = response.data.choices[0].text;
+
+  return res.send(responseText);
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}!`);
