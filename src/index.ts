ae6af79a59f6b1f9587803da298692cdcbcb133b
Yahor Piskunou <egor.piskunou@gmail.com>
2023-07-05 17:27:52 +0200

    add static hosting

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express, { Request, Response } from "express";
-import * as dotenv from "dotenv";
 import { Configuration, OpenAIApi } from "openai";
+import axios from "axios";
+import * as dotenv from "dotenv";
+import fs from "fs/promises";
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(express.json());
+app.use("/images", express.static("images"));
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -23,6 +27,59 @@ app.get("/api/getChatGPTSuggestion", async (req: Request, res: Response) => {
   const responseText = response.data.choices[0].text;
 
   return res.send(responseText);
+});
+
+app.post("/api/generateImage", async (req: Request, res: Response) => {
+  console.log(req.body);
+  const { prompt } = req.body;
+
+  const response = await openai.createImage({
+    prompt,
+    n: 1,
+    size: "1024x1024",
+  });
+
+  const imageUrl = response.data.data[0].url!;
+  const imageResponse = await axios.get(imageUrl, {
+    responseType: "arraybuffer",
+  });
+
+  const arrayBuffer: ArrayBuffer = imageResponse.data;
+
+  const timestamp = new Date().getTime();
+  const fileName = `${prompt}_${timestamp}.png`;
+  const buffer = Buffer.from(arrayBuffer);
+
+  await fs.writeFile(`images/${fileName}`, buffer);
+
+  return res.send({ body: "Successfully Uploaded Image" });
+});
+
+app.get("/api/getImages", async (req: Request, res: Response) => {
+  const files = await fs.readdir("images");
+  const imageUrls = files.map((fileName) => {
+    const url = `${req.protocol}://${req.hostname}${
+      process.env.NODE_ENV === "production" ? "" : `:${PORT}`
+    }/images/${fileName}`;
+    return {
+      url,
+      name: fileName,
+    };
+  });
+
+  const sortedImageUrls = imageUrls.sort((a, b) => {
+    const aName = Number(
+      a.name.split("_").pop()?.toString().split(".").shift()
+    );
+    const bName = Number(
+      b.name.split("_").pop()?.toString().split(".").shift()
+    );
+    return bName - aName;
+  });
+
+  return res.send({
+    imageUrls: sortedImageUrls,
+  });
 });
 
 app.listen(PORT, () => {
